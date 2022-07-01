a1525b65a1a5888ad5ab43298a3f2f2a707668c1
wendelsimoes <wendel.simoes.br@gmail.com>
2022-07-01 01:00:51 -0300

    feat: finishing implementation

--- calculator.js
+++ calculator.js
@@ -3,8 +3,37 @@ const express = require("express");
 app = express();
 const port = 3000
 
+// Body Parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    res.send("Hello, World!");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/response", function(req, res) {
+    const num1 = Number.parseFloat(req.body.num1);
+    const num2 = Number.parseFloat(req.body.num2);
+    let response = 0;
+
+    switch (req.body.operation) {
+        case "sum":
+            response = num1 + num2;
+            break;
+        case "sub":
+            response = num1 - num2;
+            break;
+        case "mult":
+            response = num1 * num2;
+            break;
+        case "div":
+            response = num1 / num2;
+            break;
+        default:
+            break;
+    }
+
+    res.send(`Your response is ${response}`);
 });
 
 app.listen(port, function() {
